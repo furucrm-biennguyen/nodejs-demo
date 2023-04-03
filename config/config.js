d28d6ee2e4800f4e236b5cec1f35efab0f47af6a
furucrm-biennguyen <bien.nguyen@furucrm.com>
2023-04-03 11:46:40 +0700

    fix ssl database config

--- config/config.js
+++ config/config.js
@@ -4,10 +4,9 @@ const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
     host: process.env.DATABASE_HOST || 'localhost',
     port: process.env.DATABASE_PORT || '5432',
-    user: process.env.DATABASE_USER || 'root',
+    user: process.env.DATABASE_USER || 'postgres',
     password: process.env.DATABASE_PASSWORD || 'root',
     database: process.env.DATABASE_NAME || 'nodejs_demo',
-    ssl: process.env.DATABASE_TSL || false,
     connectionString: process.env.DATABASE_URL,
   },
   listPerPage: process.env.LIST_PER_PAGE || 10,
