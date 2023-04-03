ba4cf38f9524d2146eb710f61d49a8916d2a034f
furucrm-biennguyen <bien.nguyen@furucrm.com>
2023-04-03 11:08:09 +0700

    fix config

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@ const dotenv = require("dotenv")
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || '5432',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'nodejs_demo',
-    ssl: process.env.DB_TSL || false,
-    connectionString: process.env.DB_URL,
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: process.env.DATABASE_PORT || '5432',
+    user: process.env.DATABASE_USER || 'root',
+    password: process.env.DATABASE_PASSWORD || 'root',
+    database: process.env.DATABASE_NAME || 'nodejs_demo',
+    ssl: process.env.DATABASE_TSL || false,
+    connectionString: process.env.DATABASE_URL,
   },
   listPerPage: process.env.LIST_PER_PAGE || 10,
 };
